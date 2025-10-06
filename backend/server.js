743a3275db0fbf301226ed94ba8fb55144c773e1
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-10-06 15:12:37 -0300

    Criando metodo POST de produtos

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,32 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 
 const PORT = 3000
 
+app.use(express.json())
+
+// Armazena produtos (simulando banco de dados)
+let produtos = []
+
+app.post('/produtos', (req,res)=>{
+    const {nome, descricao, preco, categoria} = req.body  // Colocando informacoes necessarias dentro do corpo de requisicao para serem consumidas
+    if (!nome || !descricao || !preco || !categoria){
+        res.status(400).json({mensagem: 'Todos os campos sao obrigatorios'})
+
+    }
+    const novoProduto ={
+        id: uuidv4(),
+        nome,
+        descricao,
+        preco,
+        categoria,
+    }
+    produtos.push(novoProduto)
+    res.status(201).json(novoProduto)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
