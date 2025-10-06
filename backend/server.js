bbdb432e55ae031f7a4ec798b565b79244895742
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-10-06 15:17:10 -0300

    Criando metodo Get para listar todos os produtos

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(express.json())
 // Armazena produtos (simulando banco de dados)
 let produtos = []
 
+// Criar produto 
 app.post('/produtos', (req,res)=>{
     const {nome, descricao, preco, categoria} = req.body  // Colocando informacoes necessarias dentro do corpo de requisicao para serem consumidas
     if (!nome || !descricao || !preco || !categoria){
@@ -27,6 +28,16 @@ app.post('/produtos', (req,res)=>{
     res.status(201).json(novoProduto)
 })
 
+// Buscar todos produtos
+
+app.get('/produtos', (req,res)=>{
+    res.status(200).json(novoProduto)
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
 })
+
+
+
+
